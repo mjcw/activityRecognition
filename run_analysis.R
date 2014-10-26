849d7bcdf52c9e122272cddcbcc656a4e9685b47
mjcw <mjcoursework@gmail.com>
2014-10-26 19:34:36 +0530

    Adding more comments

--- run_analysis.R
+++ run_analysis.R
@@ -1,25 +1,30 @@
 library(reshape2)
 
-# This function generates tidy data set from the Activity Recognition data set.
-# It stores the data in "tidyData.txt" file.
+#' This function generates tidy data set from the Activity Recognition data set.
+#' It stores the data in "tidyData.txt" file if outputFileName is not specified.
 tidyActivityRecognitionData <- function(outputFileName="tidyData.txt"){
-  # read cleaned activity data
+  
+  # Read cleaned activity data. readActivityRecognitionData() performs Steps 1-4 of the assignment
   activityData <- readActivityRecognitionData()
   
-  # find average
+  # 5. From the data set in step 4, creates a second, independent tidy data set with the 
+  #    average of each variable for each activity and each subject.
   columnNames = names(activityData)
   variables = columnNames[getMeanAndStdColumnNames(columnNames)]
   dataMelt <- melt(activityData, id=c("subject", "activity"), measure.vars=variables)
   tidyData <- dcast(dataMelt, subject + activity ~ variable, mean)
   
-  # write tidyData to file "tidyData.txt"
+  # write tidyData to file outputFileName
   write.table(tidyData, outputFileName, row.name=FALSE)
 }
 
-# Reads Activity Recognition data set by merging training and test data sets.
-# The function then extracts only the mean and std column names and finally
-# replaces activity ids by activity names.
-# Returns data table after performing the above mentioned operations.
+#' Reads Activity Recognition data set by merging training and test data sets.
+#' This function then extracts only the mean and std column names and finally
+#' replaces activity ids by activity names.
+#' 
+#' This function perfoms Steps 1-4 of the assignment
+#' 
+#' @return data table after performing the above listed operations.
 readActivityRecognitionData <- function(){
   
   # 1. Merges the training and the test sets to create one data set
@@ -40,28 +45,8 @@ readActivityRecognitionData <- function(){
   dataSubset
 }
 
-#' Uses regex to get  a filter for column names with "mean()" and "std()", "activity" and "subject"
-#' 
-#' @param columnNames names of all the columns
-#' @return filter with just the columns that have "mean()" and "std()", "activity" and "subject" in its name
-getSubsetColumnNames <- function(columnNames){
-  filter <- getMeanAndStdColumnNames(columnNames) | 
-    grepl("activity", columnNames) | 
-    grepl("subject", columnNames)
-  filter 
-}
-
-#' Uses regex to get  a filter for column names with "mean()" and "std()"
-#' 
-#' @param columnNames names of all the columns
-#' @return filter with just the columns that have "mean()" and "std()" in its name
-getMeanAndStdColumnNames <- function(columnNames){
-  filter <- grepl("mean\\(\\)", columnNames) | 
-    grepl("std\\(\\)", columnNames) 
-  filter
-}
-
 #' Combines Training Data set with Test Data set
+#' @return combined data set
 mergeTrainingAndTestData <- function(){
   trainingData <- readTrainingData()
   testData <- readTestData()
@@ -72,6 +57,7 @@ mergeTrainingAndTestData <- function(){
 }
 
 #' Reads the training data under the data/training directory
+#' @return Training data set
 readTrainingData <- function(){
   trainingData <- readData(dataFile = "data/train/X_train.txt", 
                    subjectsFile = "data/train/subject_train.txt", 
@@ -80,6 +66,7 @@ readTrainingData <- function(){
 }
 
 #' Reads the test data under the data/test directory
+#' @return Test data set
 readTestData <- function(){
   testData <- readData(dataFile = "data/test/X_test.txt", 
                            subjectsFile = "data/test/subject_test.txt", 
@@ -88,6 +75,7 @@ readTestData <- function(){
 }
 
 #' This function reads the data by combining dataset with the subjects and activities.
+#' It also assigns column names using data/features.txt file
 #' 
 #' @param dataFile path of the data set file
 #' @param subjectsFile path of the subjects file
@@ -112,4 +100,26 @@ readData <- function(dataFile, subjectsFile, activityLabelsFile){
   
   # return training data
   data
+}
+
+
+#' Uses regex to get a filter for column names with "mean()", "std()", "activity" and "subject"
+#' 
+#' @param columnNames names of all the columns
+#' @return filter with just the columns that have "mean()", "std()", "activity" and "subject" in its name
+getSubsetColumnNames <- function(columnNames){
+  filter <- getMeanAndStdColumnNames(columnNames) | 
+    grepl("activity", columnNames) | 
+    grepl("subject", columnNames)
+  filter 
+}
+
+#' Uses regex to get  a filter for column names with "mean()" and "std()"
+#' 
+#' @param columnNames names of all the columns
+#' @return filter with just the columns that have "mean()" and "std()" in its name
+getMeanAndStdColumnNames <- function(columnNames){
+  filter <- grepl("mean\\(\\)", columnNames) | 
+    grepl("std\\(\\)", columnNames) 
+  filter
 }
